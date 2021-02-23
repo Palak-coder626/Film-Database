8315a0e713c421a22a5880d092fd41c98d1109c7
Palak <vidhijain626@gmail.com>
2021-02-23 21:08:02 +0530

    search functionality is added

--- index.js
+++ index.js
@@ -2,33 +2,59 @@ const container=document.querySelector('.box');
 const sidebar=document.querySelector('.sidebar');
 const mainTitle=document.getElementById('main-title');
 const closeButton=document.querySelector('.close');
+const searchBar = document.querySelector('.searchbar');
+const searchBtn = document.getElementById('search');
+const searchInput=document.getElementById('query')
 const films = ["Jaws", "The Lion King","Aliens", "Harry Potter and the Philosopher's Stone", "The Goonies", "Hugo", "Twister", "Legally Blonde", "Elf", "Charlie and the Chocolate Factory"];
 let count=1;
-let movieImages=[];
 createMovieCards();
+
+function setAttributes(element,attributes){
+  for(let attribute in attributes){
+      element.setAttribute(attribute,attributes[attribute]);
+  }
+}
+
  function createMovieCards(){
      films.forEach(async (film)=>{
         const movie=await fetchMovieDetails(film);
         let card=document.createElement("span");
-        card.setAttribute('style',`--i:${count}`);
+        setAttributes(card,{style:`--i:${count}`});
         let movieImage=document.createElement('img');
-        movieImage.setAttribute('src',`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`);
-        movieImage.setAttribute('alt',`${movie.title}`);
-        movieImage.setAttribute('name',`${movie.title}`)
-        movieImage.setAttribute('title',`Click to know more..`)
+        setAttributes(movieImage,{
+            'src':`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie[0].poster_path}`,
+            'alt':`${movie[0].title}`,
+            'name':`${movie[0].title}`,
+            'class':`movie-image`,
+            'title':`Click to know more..`
+        })
         card.appendChild(movieImage);
         container.appendChild(card);
         count++;
-        console.log(movie);
     })
     
 }
+async function displaySearchResults(){
+const searchQuery = searchInput.value;
+const results = await fetchMovieDetails(searchQuery);
+const movieImages= document.querySelectorAll('.movie-image');
+for(let i=0;i<10;i++){
+    if(results[i].poster_path && results[i].vote_average>3){
+        setAttributes(movieImages[i],{
+            'src':`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${results[i].poster_path}`,
+            'alt':`${results[i].title}`,
+            'name':`${results[i].title}`
+        });
+    }
+}
+}
+
 async function fetchMovieDetails(movie){
     try{
         const url=`https://api.themoviedb.org/3/search/movie?api_key=3f1651ceb2474ae484574ca93bfebf7e&language=en-US&query=${movie}&page=1&include_adult=false`;
         const response= await fetch(url);
         const filmData=await response.json();
-        return await filmData.results[0];
+        return await filmData.results;
     }catch(e){
         console.log('Oops! Error: ',e);
     }
@@ -36,30 +62,29 @@ async function fetchMovieDetails(movie){
 
 async function generateDetails(movie){
 const filmData=await fetchMovieDetails(movie);
-console.log(filmData);
-document.querySelector('.movie-title').innerText=`${filmData.title}`;
-document.querySelector('.release-date').innerText=`Release Date : ${filmData.release_date}`;
-document.querySelector('.rating').textContent=`Rating : ${filmData.vote_average}/10`;
-document.querySelector('.sidebar > img').src=`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${filmData.poster_path}`;
-document.querySelector('.overview').innerText=`${filmData.overview}`;
+document.querySelector('.movie-title').innerText=`${filmData[0].title}`;
+document.querySelector('.release-date').innerText=`Release Date : ${filmData[0].release_date}`;
+document.querySelector('.rating').textContent=`Rating : ${filmData[0].vote_average}/10`;
+document.querySelector('.sidebar > img').src=`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${filmData[0].poster_path}`;
+document.querySelector('.overview').innerText=`${filmData[0].overview}`;
 }
 // event listeners
 document.querySelector('body').addEventListener('mouseover',(e)=>{
-if(films.includes(e.target.name)){
-    console.log(e.target.name)
+if(e.target.classList.contains('movie-image')){
     container.style.animationPlayState="paused";
 }
 });
 document.querySelector('body').addEventListener('mouseout',(e)=>{
-    if(films.includes(e.target.name)){
+    if(e.target.classList.contains('movie-image')){
         container.style.animationPlayState="running";
     }
     });
 document.querySelector('body').addEventListener('click',(e)=>{
-        if(films.includes(e.target.name)){
+        if(e.target.classList.contains('movie-image')){
             sidebar.classList.remove('hidden');
             container.style.marginRight=`${sidebar.offsetWidth}px`;            
             mainTitle.style.marginRight='40rem';
+            searchBar.style.marginRight='40rem';
             generateDetails(e.target.name);
         }
         });
@@ -67,5 +92,8 @@ closeButton.addEventListener('click',()=>{
             sidebar.classList.add('hidden');
             container.style.marginRight='0';
             mainTitle.style.marginRight='inherit';
+            searchBar.style.marginRight='inherit';
         })           
-        
+
+searchBtn.addEventListener('click',displaySearchResults);       
+searchInput.addEventListener('change',displaySearchResults);       
